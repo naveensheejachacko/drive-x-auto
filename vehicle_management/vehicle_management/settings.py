4ef4da1becea45f27d8d2838715a201a74e00585
naveen <naveensheejachacko@gmail.com>
2025-08-18 14:44:10 +0530

    Update ALLOWED_HOSTS setting to allow all hosts for development

--- vehicle_management/vehicle_management/settings.py
+++ vehicle_management/vehicle_management/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = config('SECRET_KEY', default='django-insecure-)kzy*85*60sfey1zipx(a
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = ['*']
 
 # Add render.com domains
 if not DEBUG:
